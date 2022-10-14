9c325596122e67c6e3b1a90076a58ae569e1854c
AlexVoyt <alexvoytovich@bk.ru>
2022-10-15 04:18:04 +0500

    Задание_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿//       2)Напишите программу, которая принимает на вход трёхзначное число
+//         и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите любое число:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+string str = n.ToString();
+if (n > 99)
+    Console.WriteLine(str [2]);
+else
+    Console.WriteLine("Третьей цифры нет");
+
